e2fda798a7ac7eb18c7281d2ee569b8ffa7c732f
Vasily Bishaev <pereprofessional@gmail.com>
2022-08-20 17:38:46 +0800

    fix

--- handler.php
+++ handler.php
@@ -7,17 +7,17 @@ class MadelineHandler
 
     function __construct()
     {
-        include 'mp/vendor/autoload.php';
-        include 'config.php';
+        include dirname(__FILE__).'/mp/vendor/autoload.php';
+        include dirname(__FILE__).'/config.php';
 
-        $mysql_constants = DB::showBDConstants();
+        $mysql_constants = DB::getDatabaseInfo();
         $this->mysql_connection = mysqli_connect($mysql_constants['ip'], $mysql_constants['user'], $mysql_constants['pass'], $mysql_constants['db']);
         mysqli_set_charset($this->mysql_connection, "utf8mb4");
     }
 
     public function init($session_name) // Инициализировтаь сессию (процесс авторизации или информация о юзере)
     {
-        $this->MadelineProto = new \danog\MadelineProto\API('sessions/session.'.$session_name);
+        $this->MadelineProto = new \danog\MadelineProto\API(dirname(__FILE__).'/sessions/session.'.$session_name);
         return $this->MadelineProto->start();
 
     }
--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?
-include 'mp/vendor/autoload.php';
+var_dump(scandir(dirname(__FILE__)));
+echo '<hr/>';
+echo dirname(__FILE__);
+return;
+include dirname(__FILE__).'/mp/vendor/autoload.php';
 
 if (!isset($_GET['session'])) { echo 'Надо передать GET-параметр "session" с названием сессии'; return; }
 
@@ -14,7 +18,7 @@ echo '<pre>';
 var_dump($me);
 echo '</pre>';
 
-require_once('handler.php');
+require_once(dirname(__FILE__).'/handler.php');
 $ml = new MadelineHandler();
 $ml->refreshProfileInfo($_GET['session']);
 
